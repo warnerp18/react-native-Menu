391853636fdc102f174d695c42831b76d98b1189
Phil <warnerp18@gmail.com>
2016-11-04 14:20:54 -0700

    apply stash

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,27 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 export default class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.addFish = this.addFish.bind(this);
+    this.state={
+      fishes: {},
+      order: {},
+    }
+  }
+
+  addFish(fish){
+    //update state
+      const fishes = {...this.state.fishes}
+      const timeStamp = Date.now();
+      fishes[`fish-${timeStamp}`] = fish;
+    //set state
+      this.setState({
+        fishes,
+      })
+  }
+
   render() {
     return (
       <div className='catch-of-the-day'>
@@ -12,7 +33,7 @@ export default class App extends React.Component{
           <Header tagLine="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import AddFishForm from './AddFishForm';
+
 export default class Inventory extends React.Component {
   render() {
     return (
-      <p>Header</p>
+      <div>
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
     )
   }
 }
