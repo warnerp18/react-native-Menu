7689064c31a21b15e5bd641b7c0c6ca06155aa5d
Phil <warnerp18@gmail.com>
2016-11-04 14:55:10 -0700

    create button and load sample data

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 export default class App extends React.Component{
 
   constructor(props){
     super(props);
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     this.state={
       fishes: {},
       order: {},
@@ -26,6 +28,13 @@ export default class App extends React.Component{
       })
   }
 
+  loadSamples() {
+    console.log('load sample');
+    this.setState({
+      fishes: sampleFishes,
+    });
+  }
+
   render() {
     return (
       <div className='catch-of-the-day'>
@@ -33,7 +42,7 @@ export default class App extends React.Component{
           <Header tagLine="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -8,6 +8,7 @@ export default class Inventory extends React.Component {
       <div>
         <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
   }
