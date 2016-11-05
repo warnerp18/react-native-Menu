d9c5daa187ae2d124b33bea9ede5b0654889594b
Phil <warnerp18@gmail.com>
2016-11-05 16:05:58 -0700

    display state

---
+++ src/components/Fish.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { formatPrice } from '../helpers';
+
+class Fish extends React.Component{
+
+  render() {
+    const { details } = this.props;
+    console.log(details);
+    return(
+      <li className='menu-fish'>
+      <img src={details.image} alt={details.name} />
+       <h3 className='fish-name'>
+          {details.name}
+          <span className='price'>{formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button>Add To Order</button>
+      </li>
+    )
+  }
+}
+
+export default Fish;
