a7ecbc917f953ae121512ec44a5a76a9174b6897
Phil <warnerp18@gmail.com>
2016-11-10 18:00:03 -0800

    update inventory with 2 way data/state flow

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ export default class App extends React.Component{
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.updateFish = this.updateFish.bind(this);
     this.state={
       fishes: {},
       order: {},
@@ -60,6 +61,13 @@ export default class App extends React.Component{
       })
   }
 
+  updateFish(key, updatedFishes){
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFishes;
+    this.setState({
+       fishes,
+    });
+  }
   loadSamples() {
     console.log('load sample');
     this.setState({
@@ -96,7 +104,12 @@ export default class App extends React.Component{
           order={this.state.order}
           params={this.props.params}
         />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+        />
       </div>
     )
   }
